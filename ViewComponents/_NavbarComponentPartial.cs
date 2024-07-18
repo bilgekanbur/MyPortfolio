9c4fd1c6446614a189290dedb5efb8647ccd272f
bilgekanbur <bilgesenakanbur@gmail.com>
2024-07-18 11:36:51 +0300

    Add project files.

---
+++ ViewComponents/_NavbarComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace MyPortfolio.ViewComponents
+{
+    public class _NavbarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
